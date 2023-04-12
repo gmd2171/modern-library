88bbc64cd04e0cdf0f304778e5240efcb038f8f8
Ghulam Mohiuddin <ghulammohiuddin2171@gmail.com>
2023-04-12 16:21:33 +0500

    Update axios.js

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const http = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://ec2-54-186-32-134.us-west-2.compute.amazonaws.com:3000/',
     headers: {
         "Content-type": "application/json",
     }
